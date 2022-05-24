b99659a0e4071971c2d2289397e13aa3d3ea85d2
mkl <k@caca.com>
2022-05-24 15:36:31 -0400

    first test blog with deployment of cod-zombie logs

---
+++ scripts/generate_log_to_blog.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+GITHUB_REPO="https://github.com/berlingoqc/cod-zombie-2d-clone"
+POST_FOLDER="cod-zombie-2d-clone"
+PROJECT_LOG_FOLDER="logs"
+
+function create_post_file() {
+    path=$1
+    destination=$2
+
+    cat $path > $destination
+}
+
+rm -rf $POST_FOLDER
+git clone $GITHUB_REPO
+
+for log in $(ls $POST_FOLDER/$PROJECT_LOG_FOLDER/*.md); do
+    filename=$(basename $log)
+    create_post_file $log content/$POST_FOLDER/journal/$filename
+done
