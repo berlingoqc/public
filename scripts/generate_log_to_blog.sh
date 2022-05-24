82282bc437748055bb69ec7115707d0cd328187f
mkl <k@caca.com>
2022-05-24 15:45:51 -0400

    should deploy to gh page

--- scripts/generate_log_to_blog.sh
+++ scripts/generate_log_to_blog.sh
@@ -14,9 +14,6 @@ function create_post_file() {
 rm -rf $POST_FOLDER
 git clone $GITHUB_REPO
 
-
-ls -la
-
 for log in $(ls $POST_FOLDER/$PROJECT_LOG_FOLDER/*.md); do
     filename=$(basename $log)
     create_post_file $log content/$POST_FOLDER/journal/$filename
